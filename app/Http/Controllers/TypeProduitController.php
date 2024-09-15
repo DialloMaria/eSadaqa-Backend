0152d673d995ef0ded5bca67f5cac259af077fa4
DialloMaria <diallomariam0715@gmail.com>
2024-09-15 21:41:26 +0000

    creation du model TypeProduit avec l'ensemble de ces elements

---
+++ app/Http/Controllers/TypeProduitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTypeProduitRequest;
+use App\Http\Requests\UpdateTypeProduitRequest;
+use App\Models\TypeProduit;
+
+class TypeProduitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTypeProduitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TypeProduit $typeProduit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TypeProduit $typeProduit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTypeProduitRequest $request, TypeProduit $typeProduit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TypeProduit $typeProduit)
+    {
+        //
+    }
+}
