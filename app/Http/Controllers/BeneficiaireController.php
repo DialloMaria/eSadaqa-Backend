a95404d2f697b81293c01730dce0da4fb8c22c44
DialloMaria <diallomariam0715@gmail.com>
2024-09-15 21:39:23 +0000

    creation du model Beneficiaire avec l'ensemble de ces elements

---
+++ app/Http/Controllers/BeneficiaireController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBeneficiaireRequest;
+use App\Http\Requests\UpdateBeneficiaireRequest;
+use App\Models\Beneficiaire;
+
+class BeneficiaireController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBeneficiaireRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Beneficiaire $beneficiaire)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Beneficiaire $beneficiaire)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBeneficiaireRequest $request, Beneficiaire $beneficiaire)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Beneficiaire $beneficiaire)
+    {
+        //
+    }
+}
